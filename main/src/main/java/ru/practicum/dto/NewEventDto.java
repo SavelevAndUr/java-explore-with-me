f51839e8ce59190a67b862e5954ff3627747498f
Savelev_AU <andreisavelv@yandex.ru>
2025-08-28 11:36:31 +0300

    Этап 2. Исправление тестов постман 5

--- main/src/main/java/ru/practicum/dto/NewEventDto.java
+++ main/src/main/java/ru/practicum/dto/NewEventDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -28,6 +29,7 @@ public class NewEventDto {
     @NotNull(groups = Marker.OnCreate.class)
     private LocationDto location;
     protected Boolean paid;
+    @Min(value = 0, message = "Participant limit cannot be negative")
     protected Integer participantLimit;
     protected Boolean requestModeration;
     @NotNull(groups = Marker.OnCreate.class)
